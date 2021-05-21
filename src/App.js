741223c17b831253cba694e3aa2fa7a448d8e110
alejandro <alexandros2707@gmail.com>
2021-05-21 19:19:17 -0400

    add Amplify Authenticator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css'
+import Amplify from 'aws-amplify'
+import awsConfig from './aws-exports'
+import {AmplifyAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react'
+
+Amplify.configure(awsConfig)
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Welcome to Amplify</h1>
-    </div>
+    <AmplifyAuthenticator>
+      <div className='App'>
+        <h1>Welcome to Amplify</h1>
+        <AmplifySignOut />
+      </div>
+    </AmplifyAuthenticator>
   )
 }
 
