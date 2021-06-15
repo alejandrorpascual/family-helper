720d92b3f8dd0548fea5441079e889928ec1e978
alejandro <alexandros2707@gmail.com>
2021-06-14 20:53:01 -0400

    add new item to AppSync

--- src/App.js
+++ src/App.js
@@ -8,22 +8,63 @@ import 'semantic-ui-css/semantic.min.css'
 import MainHeader from './components/headers/MainHeader'
 import Lists from './components/lists/Lists'
 import {Button, Container, Form, Icon, Modal} from 'semantic-ui-react'
+import {createList} from './graphql/mutations'
 
 Amplify.configure(awsConfig)
 
+const initialState = {
+  title: '',
+  description: '',
+}
+
+function listReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'DESCRIPTION_CHANGED': {
+      return {...state, description: action.value}
+    }
+    case 'TITLE_CHANGED': {
+      return {...state, title: action.value}
+    }
+    default:
+      console.log('Default action for', action)
+      return state
+  }
+}
+
 function App() {
+  const [state, dispatch] = React.useReducer(listReducer, initialState)
+
   const [lists, setLists] = React.useState([])
   const [isModalOpen, setIsModalOpen] = React.useState(false)
 
-  async function fetchList() {
-    const {data} = await API.graphql(graphqlOperation(listLists))
-    setLists(data.listLists.items)
-    console.log(data)
-  }
-
   React.useEffect(() => {
+    async function fetchList() {
+      try {
+        const {data} = await API.graphql(graphqlOperation(listLists))
+        setLists(data.listLists.items)
+        console.log(data)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
     fetchList()
   }, [])
+
+  async function saveList() {
+    const {title, description} = state
+    try {
+      const result = await API.graphql(
+        graphqlOperation(createList, {
+          input: {title, description},
+        }),
+      )
+      setIsModalOpen(false)
+      console.log('Save data with result:', result)
+    } catch (e) {
+      console.error(e)
+    }
+  }
 
   return (
     <AmplifyAuthenticator>
@@ -38,15 +79,26 @@ function App() {
           <Lists lists={lists} />
         </div>
       </Container>
-      <Modal open={true} dimmer='blurring'>
+      <Modal open={isModalOpen} dimmer='blurring'>
         <Modal.Header>Create your list</Modal.Header>
         <Modal.Content>
           <Form>
-            <Form.Input label='List Title' placeholder='Enter title...' />
-            <Form.TextArea
+            <Form.Input
               error={true ? false : {content: 'Please add a name to your list'}}
+              label='List Title'
+              placeholder='Enter title...'
+              value={state.title}
+              onChange={e =>
+                dispatch({type: 'TITLE_CHANGED', value: e.target.value})
+              }
+            />
+            <Form.TextArea
               label='Description'
               placeholder='Enter description...'
+              value={state.description}
+              onChange={e =>
+                dispatch({type: 'DESCRIPTION_CHANGED', value: e.target.value})
+              }
             />
           </Form>
         </Modal.Content>
@@ -54,7 +106,7 @@ function App() {
           <Button negative onClick={() => setIsModalOpen(false)}>
             Cancel
           </Button>
-          <Button positive onClick={() => setIsModalOpen(false)}>
+          <Button positive onClick={saveList}>
             Save
           </Button>
         </Modal.Actions>
